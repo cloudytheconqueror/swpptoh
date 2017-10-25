5769525cc4da66677bf0fe710026c71db279df57
cloudytheconqueror <cloudy5939@gmail.com>
2017-10-25 18:47:29 +0900

    add hero tests

--- hero/tests.py
+++ hero/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+import json
+
+from django.forms.models import model_to_dict
+from django.test import Client, TestCase
+
+from .models import Hero
 
 # Create your tests here.
+class HeroTestCase(TestCase):
+    def setUp(self):
+        Hero.objects.create(name='Superman')
+        Hero.objects.create(name='Batman')
+        Hero.objects.create(name='Joker')
+
+        self.client = Client()
+
+    def test_hero_str(self):
+        batman = Hero.objects.get(name='Batman')
+        self.assertEqual(str(batman), 'Batman')
+
+    def test_hero_detail_get(self):
+        # Test heroDetail with GET request
+        response = self.client.get('/api/hero/1')
+
+        data = json.loads(response.content.decode())  # Deserialize response data
+        self.assertEqual(data['name'], 'Superman')  # Verify the data
+        self.assertEqual(response.status_code, 200)  # Check the response code
+
